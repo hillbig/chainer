dd8084039cf80f53f29457d5185b8b59eb99e1f1
Daisuke Okanohara <hillbig@preferred.jp>
2016-01-18 13:23:50 +0900

    fix too small divider problem

--- chainer/optimizers/santa.py
+++ chainer/optimizers/santa.py
@@ -12,11 +12,11 @@ class Santa(optimizer.GradientMethod):
     See: http://arxiv.org/abs/1512.07962v1
 
     """
-    def __init__(self, eta=0.001, sigma=0.999, lambd=0.001, C=0, gamma=0.5, burnin=100):
+    def __init__(self, eta=0.001, sigma=0.999, eps=0.001, C=0.001, gamma=0.5, burnin=2000):
         # note: gamma and burnin are related. When gamma ** t -> 0 then its eqla
         self.eta = eta
         self.sigma = sigma
-        self.lambd = lambd
+        self.eps = eps
         self.C = C
         self.gamma = gamma
         self.burnin = burnin
@@ -24,37 +24,41 @@ class Santa(optimizer.GradientMethod):
     def init_state(self, param, state):
         xp = cuda.get_array_module(param.data)
         state['v'] = xp.zeros_like(param.data)
-        state['u'] = numpy.sqrt(self.eta) * numpy.random.normal(param.data.shape)
+        state['u'] = numpy.sqrt(self.eta) * numpy.random.normal(size=param.data.shape)
         state['g'] = xp.ones_like(param.data)
         state['alpha'] = xp.full_like(param.data, self.C)
 
+    def force_not_too_small(self, x):
+        return numpy.sign(x) * (numpy.abs(x) + 0.01)
+
     def update_one_cpu(self, param, state):
-        v, alpha = state['v'], state['alpha']
+        v, alpha, prev_u = state['v'], state['alpha'], state['u']
         grad = param.grad
         v *= self.sigma
         v += (1 - self.sigma) * grad * grad
 
-        g = 1 / numpy.sqrt(numpy.sqrt(v) + self.lambd)
+        g = 1 / numpy.sqrt(numpy.sqrt(v) + self.eps)
+        param.data += g * prev_u / 2
         if self.t < self.burnin:
             # exploration
-            inv_beta = 1 / (self.gamma ** self.t)
+            inv_beta = self.gamma ** self.t
             prev_g = state['g']
-            prev_u = state['u']
             alpha += (prev_u * prev_u - self.eta * inv_beta) / 2
             u = numpy.exp(-alpha/2) * prev_u
-            u += - g * grad * self.eta + numpy.sqrt(2 * prev_g * self.eta * inv_beta) * numpy.random.normal(param.data.shape) \
-                 + self.eta / beta * (1 - g / prev_g) / prev_u
+            u += - g * grad * self.eta + numpy.sqrt(2 * prev_g * self.eta * inv_beta) * numpy.random.normal(size=param.data.shape)
+            u += self.eta * inv_beta * (1 - g / self.force_not_too_small(prev_g)) / self.force_not_too_small(prev_u)
             u *= numpy.exp(-alpha/2) 
             alpha += (u * u - self.eta * inv_beta) / 2
             state['g'] = g
             state['u'] = u
         else:
             # refinement
+            u = prev_u
             u *= numpy.exp(-alpha/2)
             u -= g * grad * self.eta
             u *= numpy.exp(-alpha/2)
-        param.data -= g * u /2
-        
+        param.data += g * u /2
+
 
 
     def update_one_gpu(self, param, state):
